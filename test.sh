9b02a171dbf32683f37486dd1a8b1d54e0dd6a16
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}fullname&&${IFS}echo${IFS}') <76434077+d3kum1d0r1y4@users.noreply.github.com>
2023-07-30 20:05:43 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,15 @@
 echo "Inside File"
 echo $test
+
+my_val="$test"
+
+# Create or overwrite the file named "file"
+# Note that this will truncate the file if it already exists
+file="file"
+
+# Loop through each character in MY_VAL
+for q in $(echo "$my_val" | grep -o .); do
+    echo "$q" # Print the character
+    echo -n "$q" >> "$file" # Append the character to the file (without newline)
+done
+
